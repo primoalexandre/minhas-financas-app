bc990c0c8c933103621db89228b0be9ec1da0c29
primoalexandre <primoalexandre069@gmail.com>
2023-11-01 20:11:34 -0300

    primeiro commit do front-end

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Login from './views/login'
+import CadastroUsuario from './views/cadastroUsuario';
+
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+
+
+class App extends React.Component {
+
+
+  render(){
+    return(
+      <div>
+        <CadastroUsuario />
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Card from "../components/card"
+
+class CadastroUsuario extends React.Component{
+    render(){
+        return(
+            <Card title="Cadastro de Usuário">
+                Ola!
+            </Card>
+
+        )
+    }
+}
+export default CadastroUsuario
